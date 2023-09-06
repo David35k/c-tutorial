c1a4b260456e5949e7e063e6b3575b34323a31fd
David <davidtoman07@gmail.com>
2023-09-06 20:05:37 +1200

    constants and some silly stuff hehe

--- helloWorld.c
+++ helloWorld.c
@@ -3,13 +3,28 @@
 
 int main()
 {
+
+    // const bool doILoveMen = true;
+
+    // doILoveMen = false; // ha that gives an error you will forever love men
+
     float itemPrices[] = {5.75, 49.99, 99.99};
+    char sussy[][100] = {"Bugatti", "Food XD", "Air (per cubic centimetre)"};
 
     size_t size = sizeof(itemPrices) / sizeof(itemPrices[0]);
 
     for (int i = 0; i < size; i++)
     {
-        printf("Item %d: %8.2f\n", i + 1, itemPrices[i]);
+        printf("%s: $%8.2f\n", sussy[i], itemPrices[i]);
+    }
+
+    // GRAHH TAX!!!! NO!!!!
+    printf("NO TAX!!!! >:(\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        itemPrices[i] *= 5;
+        printf("%s: $%8.2f\n", sussy[i], itemPrices[i]);
     }
 
     return 0;
