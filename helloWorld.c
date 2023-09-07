4b225031f0ad4b8038b1d6d16fae542a9abda83a
David <davidtoman07@gmail.com>
2023-09-07 22:26:25 +1200

    sus

--- helloWorld.c
+++ helloWorld.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-    // contrinue = skips rest of code and forces the next iteration fo the loop
-    // break = exits a loop/switch
+    double prices[] = {5.0, 7.5, 3.99, 74.99, 14.99};
 
-    //count to 20 but skip 13
-    for (int i = 1; i <= 20; i++)
+    size_t size = sizeof(prices) / sizeof(prices[0]);
+
+    printf("%d elements in the array\n", size);
+
+    for (int i = 0; i < size; i++)
     {
-        if(i == 13) {
-            continue;
-        }
-        printf("%d\n", i);
+        printf("$%.2lf\n", prices[i]);
     }
+
     return 0;
 }
