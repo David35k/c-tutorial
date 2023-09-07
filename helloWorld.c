a8b2b85dfa6fdeffdeec29ee24d5fc0734df3ca4
David <davidtoman07@gmail.com>
2023-09-07 21:45:08 +1200

    while loop

--- helloWorld.c
+++ helloWorld.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
-
-void hello(char[], int); // <-- function prototype
+#include <string.h>
 
 int main()
 {
-    // function prototype
 
-    // functiond ecleration, without a body, before main()
-    // ensures that calls to a function are made with the correct arguments
+    // while loop = repeats a section of code possibly unlimited times
+    // while some condition remains true
+    // a while loop might not execute at all, depends on condition
 
-    // important notes
-    // many C compilers do not check for parameter checking
-    // missing arguments will result in unexpected behaviour
-    // a function prototype causes the compiler to flag an error if arguments are missing
+    char name[25];
 
-    // advantages
-    // 1. easier to navigate a program with main() at the top
-    // 2. helps with debugging
-    // 3. commonly used in header files
+    printf("\nWhat's your name? ");
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
 
-    char name[] = "david";
-    int age = 16;
+    while (strlen(name) == 0)
+    {
+        printf("\nwhat da frick ninja write your name!!");
+        printf("\nWhat's your name? ");
+        fgets(name, 25, stdin);
+        name[strlen(name) - 1] = '\0';
+    }
 
-    hello(name, age);
+    printf("hello ninja %s", name);
 
     return 0;
-}
-
-void hello(char name[], int age)
-{
-    printf("hello %s, you are %d years old", name, age);
 }
