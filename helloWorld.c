d04be75e29b53eee2715ff313414b512d08d5dea
David <davidtoman07@gmail.com>
2023-09-07 21:27:03 +1200

    function prototype

--- helloWorld.c
+++ helloWorld.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
-int findMax(int x, int y)
-{
-    return (x > y) ? x : y;
-}
+void hello(char[], int); // <-- function prototype
 
 int main()
 {
-    // ternary operator is shortcut for if statement when assigning or returning a value
-    // (condition) ? value if true : value if false
+    // function prototype
 
-    int max = findMax(10, 4);
+    // functiond ecleration, without a body, before main()
+    // ensures that calls to a function are made with the correct arguments
 
-    printf("%d", max);
+    // important notes
+    // many C compilers do not check for parameter checking
+    // missing arguments will result in unexpected behaviour
+    // a function prototype causes the compiler to flag an error if arguments are missing
+
+    // advantages
+    // 1. easier to navigate a program with main() at the top
+    // 2. helps with debugging
+    // 3. commonly used in header files
+
+    char name[] = "david";
+    int age = 16;
+
+    hello(name, age);
 
     return 0;
+}
+
+void hello(char name[], int age)
+{
+    printf("hello %s, you are %d years old", name, age);
 }
