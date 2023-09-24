cf286a77c0d9c083a07a9507a2772015038e4c8f
David <davidtoman07@gmail.com>
2023-09-24 18:58:28 +1300

    finished

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,4 @@
-
-        }
+if(checkFreeSpaces == 0) {
+    //     printf("It is a tie!");
+    //     return 3;
+    // }
--- ticTacToe.c
+++ ticTacToe.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 // number of rows and columns
 #define SIZE 3
@@ -47,9 +48,12 @@ int main()
         if (currentPlayer == 1)
         {
             playerMove();
+            currentPlayer = 2;
         }
         else if (currentPlayer == 2)
         {
+            computerMove();
+            currentPlayer = 1;
         }
 
         printBoard();
@@ -78,7 +82,25 @@ void printBoard()
 
         if (i != SIZE - 1)
         {
-            printf("\n-----------\n");
+            int charsNum = 4 * (SIZE - 1);
+            char str[100] = "---";
+            char ch = '-';
+
+            if (strlen(str) + charsNum >= sizeof(str))
+            {
+                printf("bruh not enough memory eks dee");
+            }
+            else
+            {
+                int ogLength = strlen(str);
+                for (int j = 0; j < charsNum; j++)
+                {
+                    str[ogLength + j] = ch;
+                }
+
+                str[ogLength + charsNum] = '\0';
+                printf("\n%s\n", str);
+            }
         }
     }
 
@@ -116,7 +138,7 @@ int checkWin()
         if (!flagHor || !flagVert)
         {
             winner = !flagHor ? checkHor[0] : checkVert[0];
-            printf("\nwinner is %c", winner);
+            printf("\nThe winner is %c!!", winner);
             return winner == 'X' ? 1 : 2;
         }
     }
@@ -141,8 +163,13 @@ int checkWin()
     if (!flagRight || !flagLeft)
     {
         winner = !flagRight ? board[0][0] : board[0][SIZE - 1];
-        printf("\nThe winner is %c!!", winner);
+        printf("\nThe winner is %c!!\n", winner);
         return winner == 'X' ? 1 : 2;
+    }
+
+    if(checkFreeSpaces() == 0) {
+        printf("\nIt's a tie! you suck lol");
+        return 3;
     }
 
     return 0;
@@ -178,10 +205,37 @@ void playerMove()
     printf("y: ");
     scanf("%d", &y);
 
+    // Makes sure to only go in empty spaces
+    while (board[y][x] != ' ')
+    {
+        printf("can't go there!\n");
+        printf("x: ");
+        scanf("%d", &x);
+        printf("y: ");
+        scanf("%d", &y);
+    }
+
     board[y][x] = 'X';
     printf("\n");
 }
 
 void computerMove()
 {
+    srand(time(0)); // set random seed to current time
+
+    printf("\nComputer move: ");
+
+    int x = rand() % SIZE;
+    int y = rand() % SIZE;
+
+    // Makes sure to only go in empty spaces
+    while (board[y][x] != ' ')
+    {
+        x = rand() % SIZE;
+        y = rand() % SIZE;
+    }
+
+    board[y][x] = 'O';
+
+    printf("(%d, %d)\n", x, y);
 }
