e37866520f9dfc02fedd8ace74e107fad90963c1
David35k <davidtoman07@gmail.com>
2023-10-05 23:08:21 +1300

    GRAHHH CANT GET IT TO WORKgit add linkedLists.c!

--- linkedLists.c
+++ linkedLists.c
@@ -1,59 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+typedef struct node {
     int value;
-    struct node *next;
-};
+    node_t *next;
+} node_t;
 
-typedef struct node node_t;
+// typedef struct node node_t;
 
 void printList(node_t *head) {
     node_t *temp = head;
 
     // n1.next = NULL so it will stop after the last element very gud
     while(temp != NULL) {
-        printf("%d - ", temp->value);
+        printf("%d ", temp->value);
         temp = temp->next;
     }
 
     printf("\n"); // pwetty fowmatting :3
 }
 
-int main() {
-    // three nodes in the list
-    node_t n1,n2,n3;
-    node_t *head; // head of the list so we know where the list starts
+node_t *createNode(int value) {
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+}
 
-    n1.value = 69;
-    n2.value = 420;
-    n3.value = 14;
+node_t *insertHead(node_t *head, node_t *node) {
+    node->next = head; // make the new node in front of current head
+    return node;
+}
 
-    // link them together
-    // going to put them in order 2,3,1 (420,14,69)
-    head = &n2;
-    n2.next = &n3;
-    n3.next = &n1;
-    n1.next = NULL; //so we know when to stop
-
-    printList(head);
-
-    // reorder them
-    head = &n1;
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = NULL;
-
-    printList(head);
-
-    // add a new node somewhere in the middle
-    node_t n4;
-    n4.value = 11;
-
-    n2.next = &n4;
-    n4.next = &n3;
+node_t *reverseList(node_t *head) {
     
+}
+
+int main() {
+    node_t *head = NULL;
+    node_t *tmp;
+
+    //this adds a new nodes at the start of the list
+    for(int i = 0;i < 10;i++) {
+        tmp = createNode(i); //create a new node
+        head = insertHead(head, tmp); // set head to the new node
+    }
+
     printList(head);
 
+    head = reverseList(head);
+
+    printList(head);
+    
     return 0;
 }
