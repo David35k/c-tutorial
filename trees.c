f90f496680eb3e6214d66fc22d8bd238ee806d49
David <davidtoman07@gmail.com>
2023-10-16 15:13:53 +1300

    sus trees

---
+++ trees.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treenode
+{
+    int value;
+    struct treenode *left;
+    struct treenode *right;
+} node;
+
+node *createNode(int value)
+{
+    node *result = malloc(sizeof(node));
+
+    if (result != NULL)
+    {
+        result->left = NULL;
+        result->right = NULL;
+        result->value = value;
+    }
+
+    return result;
+}
+
+void printTree(node *root)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
