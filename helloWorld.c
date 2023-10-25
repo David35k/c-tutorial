dd40359d37c8234b92124384d51f54fa874c4f9a
David <davidtoman07@gmail.com>
2023-10-25 13:26:48 +1300

    learning hash tables uwu

--- helloWorld.c
+++ helloWorld.c
@@ -2,25 +2,11 @@
 
 int main()
 {
-    FILE *pF = fopen("C:\\Users\\david\\OneDrive\\Desktop\\test.txt", "r");
-    char buffer[255];
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *p = arr;
 
-    // check if the file exists
-    if (pF == NULL)
-    {
-        printf("Dude, unable to open file");
-    }
-    else
-    {
-        // loops through all of the lines in the file and prints them
-        // if reaches end of file fgets will return NULL
-        while (fgets(buffer, 255, pF) != NULL)
-        {
-            printf("%s", buffer);
-        }
-    }
-
-    fclose(pF);
+    printf("%d\n", arr[2]);
+    printf("%d\n", *(p + 2));
 
     return 0;
 }
