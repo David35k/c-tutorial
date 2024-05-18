960cd5e20f227a5b171dfa0fe63c149a9b094dd4
David <davidtoman07@gmail.com>
2024-05-18 12:00:06 +1200

    infix to postfix
    very epic

--- allocatingMemory.c
+++ allocatingMemory.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
 
-    //static
+    // static
     int num = 69;
     printf("num - value: %d, size: %d bytes\n", num, sizeof(num));
 
-    //dynamic
+    // dynamic
 
     // calling malloc is asking for memory block of certain size
     // it returns pointer to the memory
     // returns NULL if cant allocate the memory
-    int *x = malloc(sizeof(int)); //memory for one int
-    int *arr = malloc(sizeof(int) * 100); //memory for 100 ints
+    int *x = malloc(sizeof(int));         // memory for one int
+    int *arr = malloc(sizeof(int) * 100); // memory for 100 ints
 
     *x = 69420;
 
     arr[50] = 100;
 
-    printf("*x - value: %d, size: %d bytes\n",*x, sizeof(*x));
+    printf("*x - value: %d, size: %d bytes\n", *x, sizeof(*x));
     printf("arr[50] - value: %d\n", arr[50]);
 
     arr[500] = 69; // OUT OF BOUNDS - BAD
-
 
     // use free to tell the allocator you dont need the memory anymore
     // good practice to set the pointer to NULL to check if empty later
@@ -38,7 +38,7 @@ int main() {
     // calloc is similar to malloc
     // intended for an array of nums
     // sets all bits of each element to 0
-    double *darr = calloc(sizeof(double), 100);
+    double *darr = calloc(100, sizeof(double));
     // same as darr = malloc(sizeof(double) * 100);
 
     darr[50] = 69.69;
@@ -47,12 +47,11 @@ int main() {
     printf("darr[50] - value: %lf, size: %d bytes\n", darr[50], sizeof(darr[50]));
     printf("this should be 0: %lf\n", darr[69]);
 
-
     // realloc lets you change the size of a block
     // THE ADDRESS OF THE NEW BLOCK COULD BE DIFFERENT!!
     // if new block cant fit it has to find new place
 
-    darr = realloc(darr, sizeof(double)*200);
+    darr = realloc(darr, sizeof(double) * 200);
 
     darr[499] = 420.69;
 
---
+++ infixToPostfix.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+
+// define the stack size
+#define STACK_SIZE 10
+
+// stack array
+char stack[STACK_SIZE];
+int top = -1; // keep track of size of arrray
+
+// adds new value to top of stack
+void push(char value)
+{
+    top++;
+
+    // check if stack overflow
+    if (top >= STACK_SIZE)
+    {
+        printf("STACK OVERFLOW!!!");
+        return;
+    }
+
+    stack[top] = value;
+}
+
+// pops topmost element off the stack
+void pop()
+{
+    if (top == -1)
+    {
+        printf("stack is already empty bro");
+        return;
+    }
+
+    top--;
+}
+
+// cheeky helped function
+int isOperator(char ch)
+{
+    switch (ch)
+    {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+    case '^':
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+// checks if c1 has precedence over c2
+// make sure c1 is always the left one
+int hasPrecedence(char c1, char c2)
+{
+    if (c1 == '^')
+    {
+        return 1;
+    }
+    else if ((c1 == '*' || c1 == '/') && c2 != '^')
+    {
+        return 1;
+    }
+    else if ((c1 == '+' || c1 == '-') && c2 != '*' && c2 != '/' && c2 != '^')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char *infixToPostfix(char *str)
+{
+    char *result = calloc(50, sizeof(char)); // final length should be the same as the original infix expression
+    // loop through the string
+    for (int i = 0; i < strlen(str); i++)
+    {
+        // if its an operand can add it straight into result
+        if (isalnum(str[i]))
+        {
+            strncat(result, &str[i], 1);
+        }
+        else if (isOperator(str[i]))
+        {
+            // keep adding from stack as long as it has precedence
+            while (hasPrecedence(stack[top], str[i]))
+            {
+                strncat(result, &stack[top], 1);
+                pop();
+            }
+
+            // add the new operand to the stack
+            push(str[i]);
+        }
+    }
+
+    // leftover operators
+    while (top > -1)
+    {
+        strncat(result, &stack[top], 1);
+        pop();
+    }
+
+    return result;
+}
+
+int main()
+{
+    char str[50];
+    printf("enter an expression in infix notation (max length 50 chars)\n");
+    fgets(str, 50, stdin);
+    printf("in postfix: %s", infixToPostfix(str));
+}
